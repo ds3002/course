dcce283be802ca0add6a583133ff74e6e0822d42
Neal Magee <nem2p@virginia.edu>
2021-04-29 10:43:59 -0400

    Lambda Cron

--- practice/14-cloud-services/lambda-cron/app.py
+++ practice/14-cloud-services/lambda-cron/app.py
@@ -6,4 +6,7 @@ app.debug = True
 
 @app.schedule('cron(* * ? * * *)')
 def cron_tab(event):
+    # 1. fetch remote file from S3 / file share / API
+    # 2. transform data 
+    # 3. write it into DB
     print(f"{datetime.now()}: I'm running!")
